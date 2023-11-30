d947f65f5a0f5f403ec2596fe1ea385addb3b1d2
l4rma <lars.magelssen@gmail.com>
2023-11-30 18:02:55 +0100

    day0

--- src/day0.rs
+++ src/day0.rs
@@ -2,23 +2,8 @@ use crate::utils::lines_from_file;
 
 pub fn main() {
     let lines = lines_from_file("inputs/day0.txt");
-    let mut calories_list: Vec<i32> = Vec::new();
-    let mut sum: i32 = 0;
     for line in lines {
-        if line == "" {
-            calories_list.push(sum);
-            sum = 0;
-            //println!("such empty");
-        } else {
-            let _calories: i32 = line.parse().unwrap();
-            sum += _calories;
-            //println!("{:?}", _calories);
-        }
+        println!("{}", line);
     }
-    // for num in &calories_list {
-    //     println!("{:?}", num)
-    // }
-    calories_list.sort();
-    println!("Highest number: {:?}", calories_list.get(calories_list.len()-1).unwrap());
 
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,8 @@
 use std::fs;
-use itertools::Itertools;
 
 pub fn lines_from_file(file_name: &str) -> Vec<String> {
     let s = fs::read_to_string(file_name).unwrap();
-    s.lines().map(|l| l.split(' ').collect()).collect_vec()
+    s.lines().map(String::from).collect()
 }
+
+
